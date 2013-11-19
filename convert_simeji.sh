36ca9db0edfd5b5237a6c179207f4016922b1322
tanjoin <armatura-quinquiplex@hotmail.co.jp>
2013-11-19 17:37:22 +0900

    v0.1.7

--- convert_simeji.sh
+++ convert_simeji.sh
@@ -1,7 +1,7 @@
 which python > /dev/null 2>&1
 if [ $? = 0 ]; then
-  echo "python : OK"
   python convert_simeji.py tanjoin_google_japanese_input.txt 
+  echo "FINISH!!"
 else
   echo "python : not found"
   echo "pythonが実行できる環境を作ってね"
